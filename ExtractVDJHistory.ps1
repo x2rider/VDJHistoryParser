1935e3d1a266627da5e465ce955694fbfd532b38
Reedy, Paul <paul.n.reedy@conocophillips.com>
2021-01-03 19:56:36 -0600

    MacOS version

--- ExtractVDJHistory.ps1
+++ ExtractVDJHistory.ps1
@@ -1,5 +1,12 @@
 $documentsDir = [Environment]::GetFolderPath("MyDocuments")
+if($documentsDir.Substring(0,1) -eq "/"){
+    #MacOS
+    $documentsDir = "$($documentsDir)/Documents"
+}
 $FilePath = "$($documentsDir)\VirtualDj\History"
+if($isMacOS){
+    $FilePath = $FilePath.Replace("\", "/")
+}
 
 $songs = @{} #song database hashtable
 $songStats = @{} #statistics used to write CSV file
@@ -25,14 +32,19 @@ function AddSongsFromDatabase([xml]$db) {
 }
 
 # list all disks
-$systemDrives = wmic logicaldisk get name | Where-Object {$_}
-#$systemDrives = @( "Name", "C:" ) # For testing only, or if you want to restrict to just one drive
+$systemDrives = [System.IO.DriveInfo]::getdrives() | Where-Object -Property TotalSize -gt 0 | Where-Object  -Property DriveType -eq "Fixed" | Where-Object -Property Name -notlike /System* | Where-Object -Property Name -notlike /private*
+#$systemDrives = @( "Name", "C:" ) # For Windows testing only, or if you want to restrict to just one drive
+#$systemDrives = @( "/" ) # For MacOS testing only, or if you want to restrict to just one drive
 
 # Read database files from each drive
 # skip first element which is just "Name"
-for ($i = 1; $i -lt $systemDrives.Count; $i++) {
-    [string]$drive = $systemDrives[$i].Trim()
-    if($drive -eq "C:"){
+#for ($i = 0; $i -lt $systemDrives.Count; $i++) {
+#    [string]$drive = $systemDrives[$i].RootDirectory.Trim()
+foreach($disk in $systemDrives) {
+    $drive = $disk.RootDirectory
+    if($drive -eq "Temp" -or $drive -eq "Name") {
+        $databaseFileName = ""
+    } elseif($drive -eq "C:\" -or $drive -eq "/"){
         # for HOME drive, database is in documents folder
         $databaseFileName = "$($documentsDir)\VirtualDj\database.xml"
     } else {
@@ -40,17 +52,25 @@ for ($i = 1; $i -lt $systemDrives.Count; $i++) {
         $databaseFileName = "$($drive)\VirtualDj\database.xml"
     }
 
+    if($isMacOS)
+    {
+        $databaseFileName = $databaseFileName.Replace("\", "/")
+    }
+
     try {
-        # If there is a VDJ database file on this drive, load it
-        if(Test-Path -Path $databaseFileName) {
-            Write-Host("Loading database $($databaseFileName)")
-            [xml]$databaseFile = Get-Content $databaseFileName
-            AddSongsFromDatabase($databaseFile)
-            Write-Host("Loading Complete")
+        if($databaseFileName.Length -gt 0) {
+            # If there is a VDJ database file on this drive, load it
+            if(Test-Path -Path $databaseFileName) {
+                Write-Host("Loading database $($databaseFileName)")
+                [xml]$databaseFile = Get-Content $databaseFileName
+                AddSongsFromDatabase($databaseFile)
+                Write-Host("Loading Complete")
+            }
         }
     }
     catch {
         Write-Host "Unable to open database file $($databaseFileName)"
+        #Write-Host $_
     }
 }
 
@@ -84,15 +104,12 @@ if($songs.Count -gt 0){
                     # Attempt to parse Artist/Title from filename
                     # Supported filename convention:
                     # Artist - Title.ext
-                    $delimiter = "\\" #Windows Path Delimiter
-                    if($songKey.LastIndexOf("\\") -lt 0){
-                        if($songKey.LastIndexOf("/") -gt 0){
-                            $delimiter = "/" # in case a mac os history file is read
-                        }
+                    $lastSlashIndex = $songKey.LastIndexOf("\")
+                    if($lastSlashIndex -lt 0){
+                        $lastSlashIndex = $songKey.LastIndexOf("/")
                     }
 
-                    [string[]]$keySplit = $songKey.split($delimiter)
-                    $tempString = $keySplit[$keySplit.Length - 1]
+                    $tempString = $songKey.Substring($lastSlashIndex+1)
                     $tempString = $tempString.split(".")[0]
 
                     [string[]]$tempStringArray = $tempString.Replace(" - ", ",").Split(",")
@@ -129,13 +146,15 @@ if($songs.Count -gt 0){
     try {
         # Write out results
         $outfile = "$($documentsDir)\VDJHistoryPlayCount.csv"
+        if($isMacOS){
+            $outfile = $outfile.Replace("\", "/")
+        }
         $songStats.Values | Sort-Object -Descending -Property PlayCount | Export-Csv -NoTypeInformation $outfile
         Write-Host "Statistics written to: $($outfile)"        
     }
     catch {
         Write-Host "Error saving CSV file, do you have it open in another program?"
     }
-
 }
 else {
     Write-Host "No songs loaded from database"
